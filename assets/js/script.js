a509702345d8030257ee214fdd2ad7559c29bd65
danielgregoire34 <danielgregoire34@gmail.com>
2022-11-05 19:43:21 -0400

    added the save feature and texting within the 9am box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,25 @@
-var calendarBlock = $('calendar-block');
-var currentDay = $('currentDay');
+var todayDate = moment().format('dddd, MMM Do YYYY');
+var timeNow = moment().hour();
+$("#currentDay").html(todayDate);
+
+
+var calendarBlock = document.getElementById('calendar-block');
+var currentDay = document.getElementById('currentDay');
+var ninebtm = document.getElementById("9am-btm");
+var ninetext = document.getElementById("9am-text");
+
+
+$(document).ready(function(){
+
+    $(".9ambtm").on("click", function () {
+        var ninetext = $(this).siblings(".description").val();
+        var ninetime = $(this).parent().attr("id");
+        localStorage.setItem(ninetime, ninetext);
+    })
+
+    $("#9am .description").val(localStorage.getItem("9am"));
+
+
+
+});
+
