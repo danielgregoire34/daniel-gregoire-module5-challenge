1530372d4b02a85594ed367e09a5eae9f994a091
danielgregoire34 <danielgregoire34@gmail.com>
2022-11-05 20:19:43 -0400

    changed html textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,34 +8,24 @@ var currentDay = document.getElementById('currentDay');
 var ninebtm = document.getElementById("9am-btm");
 var ninetext = document.getElementById("9am-text");
 
-/*
-function saveText() { 
-    let demo = document.getElementById("9amtxt").value;
-    window.localStorage.textarea = demo;
-    var dem = demo; //this is not necessary
-    document.getElementById("9amtxt").innerHTML = dem; //use demo instead
 
-} 
-*/
 $(document).ready(function(){
 
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-
-        // saving items via local storage
-        localStorage.setItem(time, text);
+        var textArea = $(this).siblings(".textarea").val();
+        var id = $(this).parent().attr("id");
+        localStorage.setItem(id, textArea);
     })
 
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("#10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+    $("#9am .textarea").val(localStorage.getItem("9am"));
+    $("#10am .textarea").val(localStorage.getItem("10am"));
+    $("#11am .textarea").val(localStorage.getItem("11am"));
+    $("#12pm .textarea").val(localStorage.getItem("12pm"));
+    $("#1pm .textarea").val(localStorage.getItem("1pm"));
+    $("#2pm .textarea").val(localStorage.getItem("2pm"));
+    $("#3pm .textarea").val(localStorage.getItem("3pm"));
+    $("#4pm .textarea").val(localStorage.getItem("4pm"));
+    $("#5pm .textarea").val(localStorage.getItem("5pm"));
 
 
 });
